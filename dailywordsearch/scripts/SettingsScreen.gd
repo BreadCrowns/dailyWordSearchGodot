ae3ab0f2afb1aab7487f5adcb0a57a40104dd2b4
BreadCrowns <BreadCrowns@users.noreply.github.com>
2025-06-13 22:39:15 -0700

    Setting Screen w Errors

--- dailywordsearch/scripts/SettingsScreen.gd
+++ dailywordsearch/scripts/SettingsScreen.gd
@@ -1,15 +1,27 @@
 extends Control
 
-@onready var back_button = $VBoxContainer/BackButton
+@onready var version_label = %VBoxContainer/InfoSection/InfoVBox/VersionLabel
+@onready var build_label = %VBoxContainer/InfoSection/InfoVBox/BuildLabel
+@onready var platform_label = %VBoxContainer/InfoSection/InfoVBox/PlatformLabel
+@onready var device_label = %VBoxContainer/InfoSection/InfoVBox/DeviceLabel
 
 func _ready():
-	back_button.pressed.connect(_on_back_pressed)
+	version_label.text = "App Version: 1.0.0"
+	build_label.text = "Build: 100001"
+	platform_label.text = "Platform: " + OS.get_name()
+	device_label.text = "Device ID: " + OS.get_unique_id()
 
-func _on_back_pressed():
-	var scene = load("res://scenes/MainMenu.tscn")
-	if scene:
-		var main_menu = scene.instantiate()
-		get_tree().root.add_child(main_menu)
-		queue_free()
-	else:
-		push_error("Failed to load MainMenu.tscn")
+func _on_HelpButton_pressed():
+	OS.shell_open("https://yourdomain.com/help")
+
+func _on_ContactButton_pressed():
+	OS.shell_open("mailto:support@yourdomain.com")
+
+func _on_PrivacyButton_pressed():
+	OS.shell_open("https://yourdomain.com/privacy")
+
+func _on_TermsButton_pressed():
+	OS.shell_open("https://yourdomain.com/terms")
+
+func _on_CreditsButton_pressed():
+	get_tree().change_scene_to_file("res://CreditsScreen.tscn")
